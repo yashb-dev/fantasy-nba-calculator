584fedf8cc6892e1dba54153c01fae0112a39a87
Yash <yashb.dev@gmail.com>
2018-12-07 08:11:32 -0500

    clean up

---
+++ build/precache-manifest.a718b09e7bf5b3ac0f19db862d58310b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8bce97456376bc31caf",
+    "url": "/static/js/main.d8bce974.chunk.js"
+  },
+  {
+    "revision": "fd1e92831869a19d9c4a",
+    "url": "/static/js/1.fd1e9283.chunk.js"
+  },
+  {
+    "revision": "d8bce97456376bc31caf",
+    "url": "/static/css/main.0b7ec021.chunk.css"
+  },
+  {
+    "revision": "fd1e92831869a19d9c4a",
+    "url": "/static/css/1.7e455d60.chunk.css"
+  },
+  {
+    "revision": "551314cbf9ac551130267ee63b5344a0",
+    "url": "/index.html"
+  }
+];
